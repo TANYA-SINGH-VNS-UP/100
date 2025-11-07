00b728d294cf1ea5a7fe92065fa6a085409d0741
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:37:38 +0530

    Update stop.go

--- internal/handlers/stop.go
+++ internal/handlers/stop.go
@@ -3,7 +3,7 @@
  *  Copyright (c) 2025 Ashok Shau
  *
  *  Licensed under GNU GPL v3
- *  See https://github.com/AshokShau/TgMusicBot
+ *  See https://github.com/TANYA-SINGH-VNS-UP/100
  */
 
 package handlers
@@ -11,10 +11,10 @@ package handlers
 import (
 	"fmt"
 
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
-	"github.com/AshokShau/TgMusicBot/internal/core/db"
-	"github.com/AshokShau/TgMusicBot/internal/lang"
-	"github.com/AshokShau/TgMusicBot/internal/vc"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/db"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/lang"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/vc"
 
 	"github.com/amarnathcjd/gogram/telegram"
 )
