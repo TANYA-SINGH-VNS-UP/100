6383a332e97b174745ff30e75e5db85a0bda5ed4
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:53:46 +0530

    Update userbot.go

--- internal/vc/userbot.go
+++ internal/vc/userbot.go
@@ -3,7 +3,7 @@
  *  Copyright (c) 2025 Ashok Shau
  *
  *  Licensed under GNU GPL v3
- *  See https://github.com/AshokShau/TgMusicBot
+ *  See https://github.com/TANYA-SINGH-VNS-UP/100
  */
 
 package vc
@@ -13,9 +13,9 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
-	"github.com/AshokShau/TgMusicBot/internal/core/db"
-	"github.com/AshokShau/TgMusicBot/internal/lang"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/db"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/lang"
 
 	"github.com/Laky-64/gologging"
 	tg "github.com/amarnathcjd/gogram/telegram"
