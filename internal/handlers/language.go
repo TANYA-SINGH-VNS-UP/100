31d96439d58ea1bf83c4208ec2281f25c06fcbbf
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:19:46 +0530

    Update language.go

--- internal/handlers/language.go
+++ internal/handlers/language.go
@@ -12,10 +12,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/AshokShau/TgMusicBot/internal/core"
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
-	"github.com/AshokShau/TgMusicBot/internal/core/db"
-	"github.com/AshokShau/TgMusicBot/internal/lang"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/db"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/lang"
 	"github.com/amarnathcjd/gogram/telegram"
 )
 
