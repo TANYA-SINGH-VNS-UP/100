e5a46206af5e8e7b06ccebf66fa45c2fddc5b0b5
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:35:33 +0530

    Update repository links and imports in speed.go

--- internal/handlers/speed.go
+++ internal/handlers/speed.go
@@ -3,7 +3,7 @@
  *  Copyright (c) 2025 Ashok Shau
  *
  *  Licensed under GNU GPL v3
- *  See https://github.com/AshokShau/TgMusicBot
+ *  See https://github.com/TANYA-SINGH-VNS-UP/100
  */
 
 package handlers
@@ -12,10 +12,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
-	"github.com/AshokShau/TgMusicBot/internal/core/db"
-	"github.com/AshokShau/TgMusicBot/internal/lang"
-	"github.com/AshokShau/TgMusicBot/internal/vc"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/db"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/lang"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/vc"
 
 	tg "github.com/amarnathcjd/gogram/telegram"
 )
