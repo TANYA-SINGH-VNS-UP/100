476edbf7e15ba408d3a994c04dd1b6a0af3fd43b
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:33:59 +0530

    Update settings.go

--- internal/handlers/settings.go
+++ internal/handlers/settings.go
@@ -3,7 +3,7 @@
  *  Copyright (c) 2025 Ashok Shau
  *
  *  Licensed under GNU GPL v3
- *  See https://github.com/AshokShau/TgMusicBot
+ *  See https://github.com/TANYA-SINGH-VNS-UP/100
  */
 
 package handlers
@@ -12,10 +12,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/AshokShau/TgMusicBot/internal/core"
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
-	"github.com/AshokShau/TgMusicBot/internal/core/db"
-	"github.com/AshokShau/TgMusicBot/internal/lang"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/db"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/lang"
 
 	"github.com/Laky-64/gologging"
 	"github.com/amarnathcjd/gogram/telegram"
@@ -58,90 +58,4 @@ func settingsHandler(m *telegram.NewMessage) error {
 		ReplyMarkup: core.SettingsKeyboard(getPlayMode, getAdminMode),
 	})
 	return err
-}
-
-func settingsCallbackHandler(c *telegram.CallbackQuery) error {
-	chatID, err := getPeerId(c.Client, c.ChatID)
-	if err != nil {
-		gologging.WarnF("getPeerId error: %v", err)
-		return nil
-	}
-	ctx, cancel := db.Ctx()
-	defer cancel()
-	langCode := db.Instance.GetLang(ctx, chatID)
-
-	// Check admin permissions
-	admins, err := cache.GetAdmins(c.Client, chatID, false)
-	if err != nil {
-		return err
-	}
-
-	var hasPerms bool
-	for _, admin := range admins {
-		if admin.User.ID == c.Sender.ID {
-			hasPerms = (admin.Rights != nil && admin.Rights.ManageCall) || admin.Status == telegram.Creator
-			break
-		}
-	}
-
-	if !hasPerms {
-		_, err := c.Answer(lang.GetString(langCode, "settings_no_permission"), &telegram.CallbackOptions{Alert: true})
-		return err
-	}
-
-	// Process the callback data
-	parts := strings.Split(c.DataString(), "_")
-	if len(parts) < 3 {
-		return nil
-	}
-
-	// Update the appropriate setting
-	settingType := parts[1]
-	settingValue := parts[2]
-
-	// Validate the setting value
-	validValues := map[string]bool{
-		cache.Admins:   true,
-		cache.Auth:     true,
-		cache.Everyone: true,
-	}
-
-	if !validValues[settingValue] {
-		_, _ = c.Answer(lang.GetString(langCode, "settings_update_invalid"), &telegram.CallbackOptions{Alert: true})
-		return nil
-	}
-
-	switch settingType {
-	case "play":
-		_ = db.Instance.SetPlayMode(ctx, chatID, settingValue)
-	case "admin":
-		_ = db.Instance.SetAdminMode(ctx, chatID, settingValue)
-	default:
-		_, _ = c.Answer(lang.GetString(langCode, "settings_update_prompt"), &telegram.CallbackOptions{Alert: true})
-		return nil
-	}
-
-	// Get updated settings
-	getPlayMode := db.Instance.GetPlayMode(ctx, chatID)
-	getAdminMode := db.Instance.GetAdminMode(ctx, chatID)
-	chat, err := c.GetChannel()
-	if err != nil {
-		gologging.WarnF("Failed to get chat: %v", err)
-		return nil
-	}
-
-	text := fmt.Sprintf(lang.GetString(langCode, "settings_header"),
-		chat.Title, getPlayMode, getAdminMode)
-
-	_, err = c.Edit(text, &telegram.SendOptions{
-		ReplyMarkup: core.SettingsKeyboard(getPlayMode, getAdminMode),
-	})
-	if err != nil {
-		gologging.WarnF("Failed to edit message: %v", err)
-		return err
-	}
-
-	_, _ = c.Answer(lang.GetString(langCode, "settings_updated"), &telegram.CallbackOptions{Alert: false})
-	_, _ = c.Edit(lang.GetString(langCode, "settings_updated"))
-	return nil
 }
