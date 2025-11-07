f732f3282d28419d40e4ca92821075b572773901
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:52:28 +0530

    Update logger.go

--- internal/vc/logger.go
+++ internal/vc/logger.go
@@ -3,7 +3,7 @@
  *  Copyright (c) 2025 Ashok Shau
  *
  *  Licensed under GNU GPL v3
- *  See https://github.com/AshokShau/TgMusicBot
+ *  See https://github.com/TANYA-SINGH-VNS-UP/100
  */
 
 package vc
@@ -11,8 +11,8 @@ package vc
 import (
 	"fmt"
 
-	"github.com/AshokShau/TgMusicBot/internal/config"
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/config"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
 
 	"github.com/Laky-64/gologging"
 	tg "github.com/amarnathcjd/gogram/telegram"
