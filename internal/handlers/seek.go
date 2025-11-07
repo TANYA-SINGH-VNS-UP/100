3dd069033ed8e010ccd86fa6a145ddff5ea13080
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-11-08 00:30:41 +0530

    Update seek.go

--- internal/handlers/seek.go
+++ internal/handlers/seek.go
@@ -12,10 +12,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/AshokShau/TgMusicBot/internal/core/cache"
-	"github.com/AshokShau/TgMusicBot/internal/core/db"
-	"github.com/AshokShau/TgMusicBot/internal/lang"
-	"github.com/AshokShau/TgMusicBot/internal/vc"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/cache"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/core/db"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/lang"
+	"github.com/TANYA-SINGH-VNS-UP/100/internal/vc"
 
 	"github.com/amarnathcjd/gogram/telegram"
 )
